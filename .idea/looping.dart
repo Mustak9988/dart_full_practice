497418c9cf50a1fef5afb3cb17ac9549c1fed063
Mustak9988 <khadem.mustak9988@gmail.com>
2022-07-04 16:14:31 +0600

    looping

---
+++ .idea/looping.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+void main() {
+
+
+
+  //general for loop
+  var nameString = 'I am Aussie';
+  for (int i = 0; i < nameString.length; i++) {
+    print('Index ${i}=> ' + nameString[i]);
+  }
+
+  //add data into list
+  int length = 1;
+  var nameList = [];
+  for (int i = 0; i < length; i++) {
+    nameList.add(stdin.readLineSync());
+    if (length == 7)
+      break;
+    else
+      length++;
+  }
+
+  // print list data through single line loop
+  nameList.reversed.forEach((element) {
+    print(element);
+  });
+
+  var collection = [1, 2, 3];
+  collection.forEach(print); // 1 2 3
+
+
+
+  //For each in diffrent way
+  for (var x in nameList) print("pirnt list value " + x);
+
+
+  //while lops
+
+  int value = 0;
+
+  while(value < 5){
+    print('value $value');
+    value++;
+  }
+
+
+  //do while loops
+  
+  print("Do while loop --> ");
+  int number = 1;
+
+  do {
+    print("  $number \n");
+    number++;
+  } while (number < 6);
+
+}
---
+++ operators.dart
@@ -0,0 +1,68 @@
+
+void main() {
+// ****************** Arithmetic Operator***************
+
+  var num1 = 10, num2 = 20;
+  print(num2 + num1);
+  print(num2 - num1);
+  print(num2 * num1);
+  print(num2 / num1);
+  print(num2 % num1);
+
+// ****************** Unary Operator***************
+
+  print('/n Unary operator');
+  int age = 10;
+  age++;
+  age += 2;
+  ++age;
+  print(age);
+
+  int a;
+  int b;
+
+  a = 0;
+  b = ++a; // Increment a before b gets its value.
+  assert(a == b); // 1 == 1
+
+  a = 0;
+  b = a++; // Increment a AFTER b gets its value.
+  assert(a != b); // 1 != 0
+
+  a = 0;
+  b = --a; // Decrement a before b gets its value.
+  assert(a == b); // -1 == -1
+
+  a = 0;
+  b = a--; // Decrement a AFTER b gets its value.
+  assert(a != b); // -1 != 0
+
+// ****************** Null aware operator ***************
+//(?.) (??) (??=)
+  print('/n Null aware operator ');
+  var number = null;
+
+  //if number is null then it will assign 100 into n
+  int n = number ?? 100;
+  print(n);
+
+
+  // ****************** Ternary operator ***************
+
+  int x = 100;
+  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  print('This is Ternary Operator $result');
+
+  // ****************** Relational operator ***************
+  //it will be discus in Conditional statement
+
+
+  //bit wise left shift, right shift and or and
+
+  assert((3 << 1) == 6); // 0011 << 1 == 0110
+  assert((3 | 4) == 7); // 0011 | 0100 == 0111
+  assert((3 & 4) == 0); // 0011 & 0100 == 0000
+
+
+
+}
